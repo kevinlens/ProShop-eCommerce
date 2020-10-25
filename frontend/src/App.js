042accc8fee53b89feb1ed706aba5db483e5da36
Kevin Tran <kevinmt191919@gmail.com>
2020-10-25 01:02:46 -0700

    Product Page with Id passed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React from 'react';
+//for pushing to router history ect
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //container moves everything to the middle
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         {/* container help centers everything */}
         <Container>
-          <HomeScreen />
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' exact component={ProductScreen} />
+
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
